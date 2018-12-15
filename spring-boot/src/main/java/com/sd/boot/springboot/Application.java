901a855ba0fbe09c9198abfa22b0f06a8c79a649
sarojdebata <sarojdebata21@gmail.com>
2018-12-15 12:25:45 +0530

    port changes and datasource class disabled

--- spring-boot/src/main/java/com/sd/boot/springboot/Application.java
+++ spring-boot/src/main/java/com/sd/boot/springboot/Application.java
@@ -1,9 +1,12 @@
 package com.sd.boot.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.sd"})
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
