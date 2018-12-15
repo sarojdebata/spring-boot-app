0bd1f211e337055e712dd235af9cbf589327cef3
sarojdebata <sarojdebata21@gmail.com>
2018-12-16 00:01:25 +0530

    Security configuration class disabled

--- spring-boot/src/main/java/com/sd/boot/springboot/Application.java
+++ spring-boot/src/main/java/com/sd/boot/springboot/Application.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages={"com.sd"})
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@ComponentScan(basePackages="com.sd.boot")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class })
 public class Application {
 
 	public static void main(String[] args) {
